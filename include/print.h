a6d3e33b2c7ea2f3ee1f8322c0272fab4303ded2
Michael Latushko <ancord.m@yandex.ru>
2019-07-08 16:05:58 +0300

    Попытки реализовать анализатор типов для кортежа

--- include/print.h
+++ include/print.h
@@ -5,6 +5,7 @@
 #include <type_traits>
 #include <vector>
 #include <list>
+#include <tuple>
 
 
 void print_ip(const std::string& ip)
@@ -13,7 +14,10 @@ void print_ip(const std::string& ip)
 }
 
 template<typename IP_TYPE>
-std::enable_if_t<std::is_integral_v<IP_TYPE>, void> print_ip(const IP_TYPE& ip)
+std::enable_if_t<
+	std::is_integral_v<IP_TYPE>,
+	void>
+print_ip(const IP_TYPE& ip)
 {
 	const unsigned char BYTE_MASK = 0xFF;
 	const unsigned char BYTE_SIZE = 8;
@@ -45,6 +49,35 @@ print_ip(const IP_TYPE& ip)
 	}
 
 	std::cout << std::endl;
+}
+
+template<typename T>
+struct are_tuple_types_same
+{
+	static const bool value = true;
+};
+
+template<typename T, typename U, typename... Args>
+struct are_tuple_types_same
+{
+	static const bool value = std::is_same_v<T, U>;
+};
+
+
+template<typename T, typename... Args>
+void print_tuple(T ip_part, Args... other_parts)
+{
+	std::cout << "Tuple" << std::endl;
+}
+
+
+template<typename... Args>
+std::enable_if_t<
+	are_tuple_types_same<Args...>::value,//std::is_same_v<IP_TYPE, std::tuple<Args...>,
+	void>
+print_ip(const std::tuple<Args...>& ip)
+{
+	print_tuple(ip);
 }
 
 #endif /* PRINT_H */
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 
 int main(int argc, const char* argv[])
 {
-	print_ip(char(-1));
+	/*print_ip(char(-1));
 	print_ip(short(0));
 	print_ip(int(2130706433));
 	print_ip(long(8875824491850138409));
 	print_ip(std::string("127.0.0.1"));
 	print_ip(std::vector<int>{192, 168, 8, 1});
-	print_ip(std::list<int>{234, 99, 31, 39, 12});
-	//print_ip(tuple)
+	print_ip(std::list<int>{234, 99, 31, 39, 720});*/
+	print_ip(std::make_tuple(12240));
 
 	return 0;
 }
